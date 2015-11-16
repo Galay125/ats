4689b15c13796412ac0b89980ae93649a2fc356a
Mário Santos <santos.mario125@gmail.com>
2015-11-16 01:33:43 +0000

    Update Main.java
    abc

--- projeto/src/geral/Main.java
+++ projeto/src/geral/Main.java
@@ -1,12 +1,39 @@
 package geral;
 
 import java.io.IOException;
+import java.util.List;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class Main {
-
+	
+	final static Charset ENCODING = StandardCharsets.UTF_8;
 	
 	  public static void main(String args[]) throws IOException {
 		  
-		  System.out.println("OLEÉÉ'!");
+		  Main text = new Main();
+		  
+		  String file = "../exemplos/fi.i";
+		  
+		  List<String> lines = text.readSmallTextFile(file);
+		  
+		  for(String line : lines){
+			  System.out.println(line);
+		  }
+
+		  
 	  }
+	  
+	  List<String> readSmallTextFile(String file) throws IOException {
+		    Path path = Paths.get(file);
+		    return Files.readAllLines(path, ENCODING);
+		  }
+	  
+	  private static void log(Object aMsg){
+		    System.out.println(String.valueOf(aMsg));
+		  }
 }
