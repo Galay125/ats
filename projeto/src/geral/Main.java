fe21543e3512b817b37054831133c17e27497e29
mario <santos.mario125@gmail.com>
2015-11-16 00:46:38 +0000

    finally

---
+++ projeto/src/geral/Main.java
@@ -0,0 +1,12 @@
+package geral;
+
+import java.io.IOException;
+
+public class Main {
+
+	
+	  public static void main(String args[]) throws IOException {
+		  
+		  System.out.println("OLEÉÉ'!");
+	  }
+}
